10594d642caf06b0fe8d4f8c774c4dceec762b5b
Yashika <yashig24004@gmail.com>
2025-07-28 13:28:32 +0530

    added mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaHdd, FaFileAlt } from 'react-icons/fa';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">ALGOVIZ</div>
-      <ul className="navbar-links">
+
+      <button className="navbar-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
         <li>
-          <Link to="/"><FaHome className="icon" /> Home</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}><FaHome className="icon" /> Home</Link>
         </li>
         <li>
-          <Link to="/visualizer"><FaHdd className="icon" /> Disk</Link>
+          <Link to="/visualizer" onClick={() => setMenuOpen(false)}><FaHdd className="icon" /> Disk</Link>
         </li>
         <li>
-          <Link to="/page-replacement"><FaFileAlt className="icon" /> Page</Link>
+          <Link to="/page-replacement" onClick={() => setMenuOpen(false)}><FaFileAlt className="icon" /> Page</Link>
         </li>
       </ul>
     </nav>
